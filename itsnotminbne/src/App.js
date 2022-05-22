46a28e9b6a56fa938915044dbc1f78db8933ba20
blankinmyhead <muo040639@gmail.com>
2022-05-22 19:10:24 +0900

    copy youtube

---
+++ itsnotminbne/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+   
+    <div>
+      <div className- "text-2x1 bg-slate-200 m-4">toss</div>
+      <div className="text 2x1 text-slate 500">toss<div/>
+      
+    </div>
+  );
+}
+
+export default App;
